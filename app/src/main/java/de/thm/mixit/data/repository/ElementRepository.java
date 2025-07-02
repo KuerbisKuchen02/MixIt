02a411c76c008fd10903725af7e63d7114122a8b
Justin Wolek <juwol1423@gmail.com>
2025-07-02 21:09:43 +0200

    feat: Implement Repository classes
    Repository classes abstract data from data-sources and contain business logic.
    
    Refs: #23

--- app/src/main/java/de/thm/mixit/data/repository/ElementRepository.java
+++ app/src/main/java/de/thm/mixit/data/repository/ElementRepository.java
@@ -1,4 +1,45 @@
 package de.thm.mixit.data.repository;
 
+import android.content.Context;
+
+import java.util.List;
+
+import de.thm.mixit.data.daos.ElementDAO;
+import de.thm.mixit.data.entities.ElementEntity;
+import de.thm.mixit.data.source.AppDatabase;
+import de.thm.mixit.data.source.ElementLocalDataSource;
+import de.thm.mixit.data.source.ICallback;
+
+/**
+ * @author Justin Wolek
+ * @version 1.0.0
+ */
 public class ElementRepository {
+    private final ElementLocalDataSource localDataSource;
+
+    public ElementRepository(ElementLocalDataSource localDataSource) {
+        this.localDataSource = localDataSource;
+    }
+
+    public static ElementRepository create(Context context) {
+        AppDatabase db = AppDatabase.getInstance(context);
+        ElementDAO dao = db.elementDAO();
+        return new ElementRepository(new ElementLocalDataSource(dao));
+    }
+
+    public void getAll(ICallback<List<ElementEntity>> callback) {
+        localDataSource.getAll(callback);
+    }
+
+    public void findById(int id, ICallback<ElementEntity> callback) {
+       localDataSource.findById(id, callback);
+    }
+
+    public void insertElement(ElementEntity element) {
+        localDataSource.insertElement(element);
+    }
+
+    public void deleteAll() {
+        localDataSource.deleteAll();
+    }
 }
