561c4b6cf03af65104e06e126fa4242683c5b89b
DerHighman <jannikheimann3@gmail.com>
2025-07-06 12:59:34 +0200

    feat: Ádd timer in arcade fragment
    #33

--- app/src/main/java/de/thm/mixit/ui/fragments/ArcadeFragment.java
+++ app/src/main/java/de/thm/mixit/ui/fragments/ArcadeFragment.java
@@ -1,13 +1,17 @@
 package de.thm.mixit.ui.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.Locale;
 
 import de.thm.mixit.R;
 
@@ -19,6 +23,28 @@ import de.thm.mixit.R;
 
 public class ArcadeFragment extends Fragment {
 
+    private static int turns = 0;
+    private Handler handler = new Handler();
+    private TextView tv_time_since_start;
+    private long startTime;
+
+    private Runnable updateTimerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long elapsedMillis = System.currentTimeMillis() - startTime;
+
+            int seconds = (int) (elapsedMillis / 1000) % 60;
+            int minutes = (int) (elapsedMillis / (1000 * 60)) % 60;
+            int hours = (int) (elapsedMillis / (1000 * 60 * 60));
+
+            String time = String.format(Locale.getDefault(), "Time: %02d:%02d:%02d", hours, minutes, seconds);
+            tv_time_since_start.setText(time);
+
+            // Alle 1 Sekunde neu aufrufen
+            handler.postDelayed(this, 1000);
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -26,4 +52,19 @@ public class ArcadeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_arcade, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        startTime = System.currentTimeMillis();
+        tv_time_since_start = view.findViewById(R.id.textview_time_since_start);
+        startTime = System.currentTimeMillis();
+        handler.post(updateTimerRunnable); // Timer starten
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        handler.removeCallbacks(updateTimerRunnable); // Handler stoppen, um Memory-Leaks zu vermeiden
+    }
 }
