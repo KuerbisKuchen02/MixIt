e97cd54988886ee18ba3094dc84b8917631edef6
DerHighman <jannikheimann3@gmail.com>
2025-07-07 14:55:28 +0200

    feat: add functionality of turn counter in ArcadeFragment
    Also add some log messages and clean up comments
    
    #24

--- app/src/main/java/de/thm/mixit/ui/fragments/ArcadeFragment.java
+++ app/src/main/java/de/thm/mixit/ui/fragments/ArcadeFragment.java
@@ -2,6 +2,7 @@ package de.thm.mixit.ui.fragments;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,13 +27,17 @@ public class ArcadeFragment extends Fragment {
     private static final String TAG = ArcadeFragment.class.getSimpleName();
     private final Handler handler = new Handler();
     private TextView tv_target_element;
-    private static String target_element;
+    private String target_element = "Schokokuchen"; // TODO replace once backend method is available
     private TextView tv_time_since_start;
-    private static long startTime;
-    private static int turns = 0;
+    private long startTime;
+    private int turns = 0;
     private TextView tv_turns;
 
 
+    /**
+     * This method is responsible to update the textview displaying the passed time.
+     * It will be called every second.
+     */
     private final Runnable updateTimerRunnable = new Runnable() {
         @Override
         public void run() {
@@ -42,10 +47,11 @@ public class ArcadeFragment extends Fragment {
             int minutes = (int) (elapsedMillis / (1000 * 60)) % 60;
             int hours = (int) (elapsedMillis / (1000 * 60 * 60));
 
-            String time = String.format(Locale.getDefault(), "Time: %02d:%02d:%02d", hours, minutes, seconds);
+            String time = String.format(Locale.getDefault(),
+                    getString(R.string.arcade_fragment_time), hours, minutes, seconds);
             tv_time_since_start.setText(time);
 
-            // Alle 1 Sekunde neu aufrufen
+            // Handler calls it again every second
             handler.postDelayed(this, 1000);
         }
     };
@@ -62,15 +68,19 @@ public class ArcadeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        startTime = System.currentTimeMillis();
+        // TODO move logic to GameViewModel once it is available
+        // Bind timer TextView and start the timer to display
         tv_time_since_start = view.findViewById(R.id.textview_time_since_start);
         startTime = System.currentTimeMillis();
-        handler.post(updateTimerRunnable); // Timer starten
+        Log.d(TAG, "Subscribe timer callback");
+        handler.post(updateTimerRunnable);
 
+        // Bind the turns TextView and set the default value
         tv_turns = view.findViewById(R.id.textview_turns);
         tv_turns.setText(String.format(Locale.getDefault(),
                 getString(R.string.arcade_fragment_turns), turns));
 
+        // Bind the target element TextView and set the default value
         tv_target_element = view.findViewById(R.id.textview_target_element);
         tv_target_element.setText(String.format(Locale.getDefault(),
                 getString(R.string.arcade_fragment_goal), target_element));
@@ -80,10 +90,14 @@ public class ArcadeFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        handler.removeCallbacks(updateTimerRunnable); // Handler stoppen, um Memory-Leaks zu vermeiden
+        Log.d(TAG, "Remove timer callback");
+        handler.removeCallbacks(updateTimerRunnable);
     }
 
-    public static void increaseTurnCounter() {
+    public void increaseTurnCounter() {
+        Log.d(TAG, "Increasing turn counter.");
         turns++;
+        tv_turns.setText(String.format(Locale.getDefault(),
+                getString(R.string.arcade_fragment_turns), turns));
     }
 }
