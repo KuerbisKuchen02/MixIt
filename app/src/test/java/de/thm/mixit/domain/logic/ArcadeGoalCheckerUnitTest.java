fc89c9b02e2f3c715223a9958b9fe017f711643a
Justin Wolek <juwol1423@gmail.com>
2025-08-07 16:24:08 +0200

    test: add unit tests for ArcadeGoalChecker
    Refs: #50

---
+++ app/src/test/java/de/thm/mixit/domain/logic/ArcadeGoalCheckerUnitTest.java
@@ -0,0 +1,47 @@
+package de.thm.mixit.domain.logic;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Contains unit tests for the ArcadeGoalChecker class.
+ *
+ * @Author Justin Wolek
+ */
+public class ArcadeGoalCheckerUnitTest {
+
+    @Test
+    public void matchesTargetElement_correctElement_returnsTrue() {
+        // Given
+        ArrayList<String> goalWords = new ArrayList<>(
+                List.of("Schokokuchen", "Schokoladenkuchen", "Schokotorte", "Schokoladentorte")
+        );
+        String word = "Schokokuchen";
+
+        // When
+        boolean isContained = ArcadeGoalChecker.matchesTargetElement(goalWords, word);
+
+        // Then
+        assertTrue(isContained);
+    }
+
+    @Test
+    public void matchesTargetElement_wrongElement_returnsFalse() {
+        // Given
+        ArrayList<String> goalWords = new ArrayList<>(
+                List.of("Schokokuchen", "Schokoladenkuchen", "Schokotorte", "Schokoladentorte")
+        );
+        String word = "Erdbeerkuchen";
+
+        // When
+        boolean isContained = ArcadeGoalChecker.matchesTargetElement(goalWords, word);
+
+        // Then
+        assertFalse(isContained);
+    }
+}
