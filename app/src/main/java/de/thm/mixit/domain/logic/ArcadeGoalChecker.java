3c01ab922575ab12233bd57226e3d71dd1eac653
Justin Wolek <juwol1423@gmail.com>
2025-08-07 16:11:09 +0200

    feat: implement ArcadeGoalChecker
    ArcadeGoalChecker checks if a given element/word matches a goal word or a list of goal word synonyms.
    
    Refs: #50

---
+++ app/src/main/java/de/thm/mixit/domain/logic/ArcadeGoalChecker.java
@@ -0,0 +1,29 @@
+package de.thm.mixit.domain.logic;
+
+import java.util.ArrayList;
+
+/**
+ * The ArcadeGoalChecker is responsible for checking if a found word/element satisfies a
+ * searched word inside the Arcade Mode.
+ *
+ * @Author Justin Wolek
+ */
+public class ArcadeGoalChecker {
+
+    /**
+     * Checks if a word is the one which is searched for inside an Arcade Session. The word
+     * is compared to multiple synonyms of the given goal word. Comparison is case-insensitive.
+     *
+     * @param goalWords An {@code ArrayList}, containing the actual goal word, as well as synonyms
+     *                  of it.
+     * @param word      The word to be checked against all goal-word variations.
+     * @return          A {@code boolean} representing if the given word is the word
+     *                  which should be found.
+     */
+    public static boolean matchesTargetElement(ArrayList<String> goalWords, String word) {
+        for (String w : goalWords) {
+            if (w.equalsIgnoreCase(word)) return true;
+        }
+        return false;
+    }
+}
