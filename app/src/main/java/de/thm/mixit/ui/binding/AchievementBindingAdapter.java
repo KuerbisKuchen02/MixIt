a12941df07bed7767c57d15c7969ccfd4ba340d8
Justin Wolek <juwol1423@gmail.com>
2025-08-29 19:50:51 +0200

    feat: implement BindingAdapter for binding ViewModel values
    Refs: #74

---
+++ app/src/main/java/de/thm/mixit/ui/binding/AchievementBindingAdapter.java
@@ -0,0 +1,27 @@
+package de.thm.mixit.ui.binding;
+
+
+import androidx.annotation.NonNull;
+import androidx.databinding.BindingAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import de.thm.mixit.data.entities.Achievement;
+import de.thm.mixit.ui.adapter.AchievementRecyclerViewAdapter;
+
+/**
+ * Data binding adapters for the AchievementsActivity
+ *
+ * @author Justin Wolek
+ */
+public class AchievementBindingAdapter {
+    @BindingAdapter("achievements")
+    public static void setAchievements(@NonNull RecyclerView recyclerView,
+                                       @NonNull List<Achievement> achievements) {
+        AchievementRecyclerViewAdapter adapter = (AchievementRecyclerViewAdapter) recyclerView.getAdapter();
+        if (adapter != null) {
+            adapter.setElements(achievements);
+        }
+    }
+}
