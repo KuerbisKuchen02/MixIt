7457454a40b5de54e0c1423ce0144fc86662eb23
jonathan.hildebrandt <jonathan.hildebrandt.03@gmail.com>
2025-08-30 13:43:02 +0200

    refactor: remove font size spinner and code cleanup
    refs: #60

--- app/src/main/java/de/thm/mixit/ui/activities/SettingsActivity.java
+++ app/src/main/java/de/thm/mixit/ui/activities/SettingsActivity.java
@@ -19,25 +19,39 @@ import java.util.Objects;
 import de.thm.mixit.R;
 import de.thm.mixit.ui.fragments.DialogResetProgress;
 
+/**
+ * Activity for the settings view.
+ * <p>
+ * This Activity implements the logic for setting the app theme and language.
+ * Also it contains the feature to reset the free play progress.
+ * </p>
+ *
+ * @author Jonathan Hildebrandt
+ */
 public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String TAG = SettingsActivity.class.getSimpleName();
 
     private Spinner spinnerTheme;
+    private Spinner spinnerLanguage;
 
     /**
-     * refs: <a href="https://developer.android.com/develop/ui/views/components/spinner">Spinners</a>
+     * Binding spinner items to spinners.
+     * ref: <a href="https://developer.android.com/develop/ui/views/components/spinner">Spinners</a>
      */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Hide Action Bar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
         setContentView(R.layout.activity_settings);
 
-        Spinner spinnerLanguage = findViewById(R.id.spinner_settings_language);
+        // Language Spinner
+
+        spinnerLanguage = findViewById(R.id.spinner_settings_language);
 
         ArrayAdapter<CharSequence> adapterLanguage = ArrayAdapter.createFromResource(
                 this, R.array.language_array, android.R.layout.simple_spinner_item
@@ -47,6 +61,33 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
 
         spinnerLanguage.setAdapter(adapterLanguage);
 
+        preselectLanguageSpinner();
+
+        // Theme Spinner
+
+        spinnerTheme = findViewById(R.id.spinner_settings_theme);
+
+        ArrayAdapter<CharSequence> adapterTheme = ArrayAdapter.createFromResource(
+                this, R.array.theme_array, android.R.layout.simple_spinner_item
+        );
+
+        adapterTheme.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerTheme.setAdapter(adapterTheme);
+
+        // set initial selection based on current mode
+        preselectThemeSpinner();
+
+        spinnerLanguage.setOnItemSelectedListener(this);
+        spinnerTheme.setOnItemSelectedListener(this);
+
+        Log.i(TAG, "SettingsActivity was created");
+    }
+
+    /**
+     * This method preselects the language spinner based on the set app language.
+     */
+    private void preselectLanguageSpinner() {
         LocaleListCompat currentLocales = AppCompatDelegate.getApplicationLocales();
 
         if (!currentLocales.isEmpty()) {
@@ -64,54 +105,37 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
                     break;
             }
         }
-
-        // Theme Spinner
-        spinnerTheme = findViewById(R.id.spinner_settings_theme);
-        ArrayAdapter<CharSequence> adapterTheme = ArrayAdapter.createFromResource(
-                this, R.array.theme_array, android.R.layout.simple_spinner_item
-        );
-        adapterTheme.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerTheme.setAdapter(adapterTheme);
-
-        // set initial selection based on current mode
-        preselectThemeSpinner();
-
-        // Font Size Spinner
-        Spinner spinnerFontSize = findViewById(R.id.spinner_settings_font_size);
-        ArrayAdapter<CharSequence> adapterFontSize = ArrayAdapter.createFromResource(
-                this, R.array.font_size_array, android.R.layout.simple_spinner_item
-        );
-        adapterFontSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerFontSize.setAdapter(adapterFontSize);
-
-        spinnerLanguage.setOnItemSelectedListener(this);
-        spinnerTheme.setOnItemSelectedListener(this);
-        spinnerFontSize.setOnItemSelectedListener(this);
-
-        Log.i(TAG, "SettingsActivity was created");
     }
 
     /**
-     * refs: <a href="https://developer.android.com/develop/ui/views/theming/darktheme#change-themes">...</a>
+     * This Method preselects the theme spinner based on the uiModeManager mode.
+     * ref:
+     * <a href="https://developer.android.com/develop/ui/views/theming/darktheme#change-themes">
+     *     In app dark theme changes
+     * </a>
      */
     private void preselectThemeSpinner() {
-        int selection = 0; // default: system
+        int selection = 0; // System theme
         if (Build.VERSION.SDK_INT >= 31) {
             UiModeManager uiModeManager = getSystemService(UiModeManager.class);
             int mode = uiModeManager != null ? uiModeManager.getNightMode()
                     : UiModeManager.MODE_NIGHT_AUTO; // System
-            if (mode == UiModeManager.MODE_NIGHT_NO) selection = 1;      // Light
+            if (mode == UiModeManager.MODE_NIGHT_NO) selection = 1; // Light
             else if (mode == UiModeManager.MODE_NIGHT_YES) selection = 2; // Dark
         } else {
             int mode = AppCompatDelegate.getDefaultNightMode();
-            if (mode == AppCompatDelegate.MODE_NIGHT_NO) selection = 1;       // Light
+            if (mode == AppCompatDelegate.MODE_NIGHT_NO) selection = 1; // Light
             else if (mode == AppCompatDelegate.MODE_NIGHT_YES) selection = 2; // Dark
         }
         spinnerTheme.setSelection(selection, false);
     }
 
     /**
-     * refs: <a href="https://developer.android.com/develop/ui/views/theming/darktheme#change-themes">...</a>
+     * This Method applies the selected spinner item to the app theme
+     * ref:
+     * <a href="https://developer.android.com/develop/ui/views/theming/darktheme#change-themes">
+     *     In app dark theme changes
+     * </a>
      */
     private void applyTheme(String themeValue) {
         if (Build.VERSION.SDK_INT >= 31) {
@@ -146,7 +170,6 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
 
     public void onResetProgressClicked(View view) {
         Log.i(TAG, "Reset Progress Button was clicked.");
-
         new DialogResetProgress().show(getSupportFragmentManager(), "DialogResetProgress");
     }
 
@@ -155,25 +178,29 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         int viewId = parent.getId();
 
         if (viewId == R.id.spinner_settings_language) {
-            String[] values = getResources().getStringArray(R.array.language_values); // "system", "en", "de"
+            // "system", "en", "de"
+            String[] values = getResources().getStringArray(R.array.language_values);
             String selectedLangTag = values[position];
 
             Log.d(TAG, "Language selected (tag): " + selectedLangTag);
 
-            // if local is system clear locale list if not set it to selected locale
+            // if local is system, clear locale list if not set it to selected locale
             LocaleListCompat locales = "system".equals(selectedLangTag)
                     ? LocaleListCompat.getEmptyLocaleList()
                     : LocaleListCompat.forLanguageTags(selectedLangTag);
 
-            // refs: https://developer.android.com/guide/topics/resources/app-languages#androidx-impl
+            // ref:
+            // https://developer.android.com/guide/topics/resources/app-languages#androidx-impl
             AppCompatDelegate.setApplicationLocales(locales);
         } else if (viewId == R.id.spinner_settings_theme) {
-            String[] values = getResources().getStringArray(R.array.theme_values); // "system","light","dark"
+            // "system","light","dark"
+            String[] values = getResources().getStringArray(R.array.theme_values);
+
             String selectedTheme = values[position];
-            Log.d(TAG, "Theme selected: " + selectedTheme);
+
             applyTheme(selectedTheme);
-        } else if (viewId == R.id.spinner_settings_font_size) {
-            Log.d(TAG, "Font size selected: " + parent.getItemAtPosition(position));
+
+            Log.d(TAG, "Theme selected: " + selectedTheme);
         }
     }
 
