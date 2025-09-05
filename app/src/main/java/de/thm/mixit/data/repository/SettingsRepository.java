3eae579600ea8a6a21f6c1cb84fcb809e55c578d
DerHighman <jannikheimann3@gmail.com>
2025-09-05 11:07:36 +0200

    feat: Add Settings- entity, datasource and repo
    #109

---
+++ app/src/main/java/de/thm/mixit/data/repository/SettingsRepository.java
@@ -0,0 +1,59 @@
+package de.thm.mixit.data.repository;
+
+import android.content.Context;
+
+import de.thm.mixit.data.entities.Settings;
+import de.thm.mixit.data.source.SettingsDataSource;
+
+/**
+ * Repository class that provides access to saved Settings data.
+ * <p>
+ * Acts as a single source of truth for Settings data by delegating
+ * data operations to a {@link SettingsDataSource}.
+ *
+ * @author Jannik Heimann
+ */
+public class SettingsRepository {
+
+    private final SettingsDataSource datasource;
+
+    private SettingsRepository(SettingsDataSource datasource) {
+        this.datasource = datasource;
+    }
+
+    /**
+     * Method to create an instance of the class.
+     * @param context Context of the Android application.
+     * @return {@link SettingsRepository}
+     *
+     * @author Jannik Heimann
+     */
+    public static SettingsRepository create(Context context) {
+        return new SettingsRepository(new SettingsDataSource(context));
+    }
+
+    /**
+     * Loads the last saved Settings by calling the load Method in the corresponding datasource.
+     * @return {@link Settings}
+     *
+     * @author Jannik Heimann
+     */
+    public Settings loadSettings() {
+        return datasource.loadSettings();
+    }
+
+    /**
+     * Saves the given Settings by calling the save Method in the corresponding datasource.
+     *
+     * @author Jannik Heimann
+     */
+    public void saveSettings(Settings settings) {
+        datasource.saveSettings(settings);
+    }
+
+
+    /**
+     * Deletes the saved Settings.
+     */
+    public void deleteSavedSettings() { datasource.deleteSavedSettings(); }
+}
